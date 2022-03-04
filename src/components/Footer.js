64910de746485c7ab1d4231d6f385423cd9e1f4a
Maanvendra Singh <maansingh2618@gmail.com>
2022-03-04 17:30:41 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(){
         <div className="footer">
             <a href='https://www.facebook.com' target="_blank" rel="noreferrer"><img src={facebook} alt="facebook-logo"/></a>
             <a href='https://twitter.com/maansinghh' target="_blank" rel="noreferrer"><img src={twitter} alt="twitter-logo" /></a>
-            <a href='https://www.instagram.com/_m.a.a.n_/' target="_blank" rel="noreferrer"><img src={insta} alt="instagram-logo"/></a>
+            <a href='https://www.instagram.com/maan.eth/' target="_blank" rel="noreferrer"><img src={insta} alt="instagram-logo"/></a>
             <a href='https://github.com/maanvendrasingh' target="_blank" rel="noreferrer"><img src={github} alt="github-logo" /></a>
         </div>
     )
